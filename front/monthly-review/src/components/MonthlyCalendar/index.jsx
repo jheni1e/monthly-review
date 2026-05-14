635ee1920424ec561beecc94d4f52eef6de06659
Jhenie <highhalma@gmail.com>
2026-05-14 14:19:26 -0300

    new version of the calendar, hover on the dropdown and create button are the same now

--- front/monthly-review/src/components/MonthlyCalendar/index.jsx
+++ front/monthly-review/src/components/MonthlyCalendar/index.jsx
@@ -1,14 +1,62 @@
+import { useState } from "react";
 import DayCell from "../DayCell";
-import { mockEvents } from "../../mocks/mockEvents";
 import "./index.css";
+import DropdownList from "../../components/DropdownList";
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 export default function MonthlyCalendar() {
-  const days = Array.from({ length: 31 }, (_, i) => i + 1);
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth();
+
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  const firstDayOfMonth = new Date(year, month, 1).getDay();
+
+  const emptyCells = Array.from({ length: firstDayOfMonth });
+
+  const days = Array.from({ length: daysInMonth }, (_, i) => i + 1);
+
+  const monthOptions = months.map((monthName, index) => ({
+    label: monthName,
+    value: index,
+  }));
 
   return (
     <div className="calendar-container">
       <div className="calendar-header">
-        <h1>January 2026</h1>
+        <div>
+          <h1>
+            {months[month]} {year}
+          </h1>
+        </div>
+
+        <div className="actions">
+          <DropdownList
+            options={monthOptions}
+            value={month}
+            onChange={(e) =>
+              setCurrentDate(new Date(year, Number(e.target.value), 1))
+            }
+          />
+
+          <button>Create</button>
+        </div>
       </div>
 
       <div className="weekdays">
@@ -22,8 +70,12 @@ export default function MonthlyCalendar() {
       </div>
 
       <div className="calendar-grid">
+        {emptyCells.map((_, index) => (
+          <div key={`empty-${index}`} className="empty-cell" />
+        ))}
+
         {days.map((day) => (
-          <DayCell key={day} day={day} events={mockEvents} />
+          <DayCell key={day} day={day} events={[]} />
         ))}
       </div>
     </div>
